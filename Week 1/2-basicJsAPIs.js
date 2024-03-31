69bcf30f2f3d9126c72a092695e6ef3a15c867d5
amankumbhalwar <amankumbhalwar09@gmail.com>
2024-03-31 21:23:26 +0530

    Methods

--- Week 1/2-basicJsAPIs.js
+++ Week 1/2-basicJsAPIs.js
@@ -133,6 +133,8 @@ console.log("===NUMBERS METHODS COMPLETED===");
 
 */
 
+/*
+
 console.log("===ARRAYS METHODS===");
 function pushExample(arr, element) {
   console.log("Original Array: " + arr);
@@ -169,11 +171,13 @@ console.log("====================================");
 const initialArray = [1, 2, 3, 4, 5, 6];
 
 function logThing(str) {
-  console.log(str+ " is a string");
+  console.log(str + " is a string");
 }
 initialArray.forEach(logThing);
 
 console.log("====================================");
+
+*/
 
 console.log("Important Note: callbacks function...");
 // function log1() {
@@ -193,6 +197,177 @@ console.log("Important Note: callbacks function...");
 console.log("====================================");
 
 console.log("callbacks, map, filter, find, sort to be continued...");
+
+console.log("====================================");
+
+/*
+console.log("Best Practice Class and Object...");
+
+// Class is a blueprint for creating objects with properties and methods.
+class Animal {
+  constructor(name, legCount, speaks) {
+    this.name = name;
+    this.legCount = legCount;
+    this.speaks = speaks;
+  }
+  // static method
+  static myType() {
+    return "I'm an animal class";
+  }
+  speak() {
+    console.log(this.name + " says " + this.speaks);
+  }
+}
+
+// Creating a object of class Animal
+const dog = new Animal("Dog", 4, "Bhow Bhow"); // create object
+const cat = new Animal("Cat", 4, "Meow Meow"); // create object
+const parrot = new Animal("Parrot", 2, "Talk"); // create object
+
+console.log(dog); // return the object
+console.log(dog.name); // return the name of object
+dog.speak();
+cat.speak(); // call function on object
+
+// without instantiation the object we can call static method
+console.log(Animal.myType()); // return the class
+
+console.log("====================================");
+
+*/
+
+/*
+console.log("How to calculate a Date & Time in JavaScript...");
+
+const date = new Date();
+
+console.log("Date " + date.getDate()); // return the date
+console.log("Month " + date.getMonth()); // return the month i.e month starts from 0
+console.log("Year " + date.getFullYear()); // return the year
+
+console.log("Hours " + date.getHours()); // return the hours
+console.log("Minutes " + date.getMinutes()); // return the minutes
+console.log("Seconds " + date.getSeconds()); // return the seconds
+
+console.log("Weekday " + date.getDay()); // return the day of the week i.e weekday starts from 0
+
+// Setting components of the date
+date.setFullYear(2022);
+console.log("After setFullYear " + date);
+
+console.log("Time in milliseconds since 1970 " + date.getTime()); // return the time in milliseconds i.e epoch timestamp
+
+*/
+
+/*
+function calculateSum(){
+  let sum = 0;
+  for(let i=0; i<100000000; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+const beforeDate = new Date();
+const beforeTimeInMs = beforeDate.getTime();
+
+calculateSum();
+
+const afterDate = new Date();
+const afterTimeInMs = afterDate.getTime();
+
+console.log("Time taken to calculate sum in milliseconds " + (afterTimeInMs - beforeTimeInMs) + " ms");
+
+*/
+console.log("JSON.stringify & JSON.parse");
+
+/*
+// JSON
+const users1 = '{"userName":"Aman","age":27,"location":"Bangalore"}'; // use JSON.parse
+const users2 = {
+  userName: "Aman",
+  age: 27,
+  location: "Bangalore",
+} // use JSON.stringify
+
+// JSON.parse() - this returns object
+// JSON.stringify() - this returns string
+
+const printUser1 = JSON.parse(users1);
+console.log(printUser1); // { userName: 'Aman', age: 27, location: 'Bangalore' }
+console.log(printUser1.userName); // Aman
+
+const printUser2 = JSON.stringify(users2); 
+console.log(printUser2); // {"userName":"Aman","age":27,"location":"Bangalore"}
+console.log(printUser2.userName); // undefined -> because it is a string
+
+console.log(users2.userName ); // Aman
+
+
+*/
+console.log("====================================");
+
+/*
+
+// Maths
+
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.pow(2, 3)); // 8
+
+console.log(Math.sqrt(9)); // 3
+
+console.log(Math.abs(-10)); // 10
+
+console.log(Math.max(10, 20)); // 20
+
+console.log(Math.min(10, 20)); // 10
+
+console.log(Math.floor(10.9)); // 10
+
+console.log(Math.ceil(10.9)); // 11
+
+console.log(Math.round(10.9)); // 10
+
+console.log(Math.random() * 10); // 0 to 10
+
+*/
+
+// Object Methods Explanation
+function objectMethods(obj) {
+  console.log("Original Object:", obj);
+
+  let keys = Object.keys(obj);
+  console.log("After Object.keys():", keys);
+
+  let values = Object.values(obj);
+  console.log("After Object.values():", values);
+
+  let entries = Object.entries(obj);
+  console.log("After Object.entries():", entries);
+
+  let hasProp = obj.hasOwnProperty("key1");
+  console.log("After hasOwnProperty():", hasProp);
+
+  let newObj = Object.assign({}, obj, { newProperty: "newValue" });
+  console.log("After Object.assign():", newObj);
+
+
+}
+
+// Example Usage for Object Methods
+const sampleObject = {
+  key1: "value1",
+  key2: "value2",
+  key3: "value3",
+};
+
+objectMethods(sampleObject);
+
+
+
+
+
 
 
 
