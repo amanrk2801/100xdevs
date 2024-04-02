b8495e8e9fa451588ee718b1e11c08d9480bfa6d
amankumbhalwar <amankumbhalwar09@gmail.com>
2024-04-02 16:07:10 +0530

    bash

---
+++ Week 2/3-bash.js
@@ -0,0 +1,35 @@
+// Bash and terminal
+
+// Bash
+// 1. pwd
+// 2. cd
+// 3. ls
+// 4. mkdir
+// 5. touch
+// 6. cat
+// 7. vi
+// 8. mv
+// 9. cp (for copying files) or cp -r test3 test2 (for copying folders)
+// 10. nvm
+// 11. npm install express
+// 12. node
+
+// Bash (Advance) see video
+// 1. pwd
+// 2. ls
+// 3. cat
+// 4. touch
+// 5. mkdir
+// 6. mv
+// 7. cp
+// 8. rm
+// 9. chmod
+// 10. echo
+// 11. head and tail
+// 12. pipe operator
+// 13. wc
+// 14. grep
+// 15. history
+// 16. script in bash
+// 17. grep, sed, awk
+// 18. find
