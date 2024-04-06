551d6a063ea9aba2d2de2588b77b5ec6290bb162
amankumbhalwar <amankumbhalwar09@gmail.com>
2024-04-06 10:14:51 +0530

    5.1 upload

--- Week 5/React 1/count/src/App.jsx
+++ Week 5/React 1/count/src/App.jsx
@@ -1,45 +1,53 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([{
-    title: "Go to gym",
-    description: "Go to gym from 7-9",
-    completed: false
-  }, {
-    title: "Study DSA",
-    description: "Study DSA form 9-11",
-    completed: true
-  }, {
-    title: "Study React",
-    description: "Study React form 11-1",
-    completed: true
-  }, ]); 
+  const [todos, setTodos] = useState([
+    {
+      title: "Go to gym",
+      description: "Go to gym from 7-9",
+      completed: false,
+    },
+    {
+      title: "Study DSA",
+      description: "Study DSA form 9-11",
+      completed: true,
+    },
+    {
+      title: "Study React",
+      description: "Study React form 11-1",
+      completed: true,
+    },
+  ]);
 
   function addTodo() {
     // [1, 2]
     // [...todos, 3] => [1, 2, 3]
-    setTodos([...todos, {
-      title: "new Todo",
-      description: "desc of new todo"
-    }])
+    setTodos([
+      ...todos,
+      {
+        title: "new Todo",
+        description: "desc of new todo",
+      },
+    ]);
   }
 
   return (
     <div>
       <button onClick={addTodo}>Add a random todo</button>
-      {todos.map(function(todo) {
-        return <Todo title={todo.title} description={todo.description} />
+      {todos.map(function (todo) {
+        return <Todo title={todo.title} description={todo.description} />;
       })}
-
     </div>
-  )
+  );
 }
 
 function Todo(props) {
-  return <div>
-    <h1>{props.title}</h1>
-    <h2>{props.description}</h2>
-  </div>
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
