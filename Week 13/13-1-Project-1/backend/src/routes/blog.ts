606bbe271dfc64a794bc86bd8e95bc9cf1dbf1c4
amankumbhalwar <amankumbhalwar09@gmail.com>
2024-04-15 18:10:59 +0530

    medium-backend

---
+++ Week 13/13-1-Project-1/backend/src/routes/blog.ts
@@ -0,0 +1,147 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
+import { createPostInput, updatePostInput } from "@amankumbhalwar/common-medium";
+
+export const blogRouter = new Hono<{
+  // this required for removing error in c.env.DATABASE_URL
+  Bindings: {
+    DATABASE_URL: string; // present in wrangler.toml
+    JWT_SECRET: string; // // present in wrangler.toml
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+  // extract the userId
+  // pass it down to the route handler
+
+  // get the header
+  // verify the header
+  // if the header is correct, we need can proceed
+  // if not, we return the user a 403 status code
+  const authHeader = c.req.header("Authorization") || "";
+  if (!authHeader) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = await verify(token, c.env.JWT_SECRET);
+    if (!payload) {
+      c.status(401);
+      return c.json({ error: "unauthorized" });
+    }
+    c.set("userId", payload.id);
+    await next();
+  } catch (e) {
+    c.status(403);
+    return c.json({
+      message: "You are not logged in",
+    });
+  }
+});
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = createPostInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct",
+    });
+  }
+  const authorId = c.get("userId");
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: authorId,
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = updatePostInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct",
+    });
+  }
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.post.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+
+/*NOTE: 
+blogRouter.get("/bulk", async (c)) this should
+    be above than this 
+blogRouter.get("/:id", async (c)
+    otherwise cause error
+*/
+
+// TODO : add pagination
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blogs = await prisma.post.findMany();
+
+  return c.json({
+    blogs,
+  });
+});
+
+blogRouter.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    return c.json({
+      blog,
+    });
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.json({
+      message: "Error while fetching blog post",
+    });
+  }
+});
---
+++ Week 13/13-1-Project-1/backend/src/routes/user.ts
@@ -0,0 +1,83 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+import { signupInput, signinInput } from "@amankumbhalwar/common-medium";
+
+export const userRouter = new Hono<{
+  // this required for removing error in c.env.DATABASE_URL
+  Bindings: {
+    DATABASE_URL: string; // present in wrangler.toml
+    JWT_SECRET: string; // // present in wrangler.toml
+  };
+}>();
+
+// c => context(req,res)
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct",
+    });
+  }
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({
+      jwt: token,
+    });
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct",
+    });
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403); // 403=>Unauthorized
+      return c.json({ error: "user not found" });
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
