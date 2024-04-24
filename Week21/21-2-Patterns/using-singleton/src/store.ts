1e5e3fd7d0bcbafd215bcc096a5b3072671e56af
amankumbhalwar <amankumbhalwar09@gmail.com>
2024-04-24 20:39:46 +0530

    Patterns and PubSubs

---
+++ Week21/21-2-Patterns/using-singleton/src/store.ts
@@ -0,0 +1,44 @@
+interface Game {
+  id: string;
+  whitePlayerName: string;
+  blackPlayerName: string;
+  moves: string[];
+}
+
+// Singleton class
+// static variables and static methods
+export class GameManager {
+  games: Game[] = [];
+  private static instance : GameManager;
+  private constructor() {
+    this.games = [];
+  }
+
+  static getInstance(){
+    // create a single instance of GameManager and return it
+    if(GameManager.instance){
+      return GameManager.instance
+    }
+    GameManager.instance= new GameManager();
+    return GameManager.instance
+  }
+
+  addMove(gameId: string, move: string) {
+    console.log(`Adding move ${move} to game ${gameId}`);
+    const game = this.games.find((game) => game.id === gameId);
+    game?.moves.push(move);
+  }
+  addGame(gameId: string) {
+    const game = {
+      id: gameId,
+      whitePlayerName: "Alice",
+      blackPlayerName: "Denzel",
+      moves: [],
+    };
+    this.games.push(game);
+  }
+
+  log(){
+    console.log(this.games); 
+  }
+}
