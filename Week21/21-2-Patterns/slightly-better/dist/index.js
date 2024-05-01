850f7fe2df28e0dbcd779b9dab9a7c29dcdaf6ec
amanrk2801 <amankumbhalwar09@gmail.com>
2024-05-01 18:45:21 +0530

    changes

--- Week21/21-2-Patterns/slightly-better/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const store_1 = require("./store");
-const logger_1 = require("./logger");
-(0, logger_1.startLogger)();
-setInterval(() => {
-    store_1.gameManager.addGame(Math.random.toString());
-}, 5000);
